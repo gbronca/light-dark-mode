4ce12f07e3174e58ab4004da1d088d74ad8ebc3d
gbronca <42703789+gbronca@users.noreply.github.com>
2020-08-19 15:59:57 +0100

    minor code improvements

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
@@ -19,18 +22,18 @@ function toggleDarkLightMode(isDark) {
 	toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
 	isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
 		toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-	isDark ? imageMode('dark') : imageMode('light');
+	isDark ? imageMode(DARK_THEME) : imageMode(LIGHT_THEME);
 }
 
 //Switch Theme Dynamically
 function switchTheme(event) {
 	if (event.target.checked) {
-		document.documentElement.setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark');
+		document.documentElement.setAttribute('data-theme', DARK_THEME);
+		localStorage.setItem('theme', DARK_THEME);
 		toggleDarkLightMode(true);
 	} else {
-		document.documentElement.setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light');
+		document.documentElement.setAttribute('data-theme', LIGHT_THEME);
+		localStorage.setItem('theme', LIGHT_THEME);
 		toggleDarkLightMode(false);
 	}
 }
@@ -43,7 +46,7 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
 	document.documentElement.setAttribute('data-theme', currentTheme);
 
-	if (currentTheme === 'dark') {
+	if (currentTheme === DARK_THEME) {
 		toggleSwitch.checked = true;
 		toggleDarkLightMode(true);
 	}
